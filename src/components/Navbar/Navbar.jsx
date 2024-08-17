f46af2c61c6f72b611fdaf768260785998524be6
Jhalak <stdjhalak@gmail.com>
2024-08-17 18:42:31 +0600

    final commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
             <div className="container px-6 py-3 mx-auto md:flex">
                 <div className="text-white flex items-center">
                     <h3 className="text-xl w-44">Catalog Master</h3>
+                    
 
                     {/* Mobile menu button */}
                     <div className="flex lg:hidden md:hidden">
@@ -49,8 +50,8 @@ const Navbar = () => {
                 {/* Mobile Menu */}
                 <div className={`${isOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full'} absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-blue-950 dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between`}>
                     <div className="flex items-center flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0">
-                        <a href="/" className="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-500 md:mx-2">Home</a>
-                        <a onClick={()=>scrollToSection('collection')} className="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-500 md:mx-2 cursor-pointer">Our Products Collection</a>
+                        <Link to="/" className="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-500 md:mx-2">Home</Link>
+                        <Link to="" onClick={()=>scrollToSection('collection')} className="px-2.5 py-2 text-white transition-colors duration-300 transform rounded-lg hover:text-gray-500 md:mx-2 cursor-pointer">Our Products Collection</Link>
                        
                         {
                             user ?
